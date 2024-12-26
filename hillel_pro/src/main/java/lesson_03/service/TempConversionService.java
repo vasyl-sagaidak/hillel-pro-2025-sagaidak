c741513e705d82ee602ff26bc64e2163ca3a862d
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-27 00:59:53 +0200

    both side C to F converter implemented

--- hillel_pro/src/main/java/lesson_03/service/TempConversionService.java
+++ hillel_pro/src/main/java/lesson_03/service/TempConversionService.java
@@ -3,6 +3,7 @@ package lesson_03.service;
 public class TempConversionService {
     private static final double TEMP_DIFF = 32.0;
     private static final double ABS_MIN_CELS = -273.15;
+    private static final double ABS_MIN_FAHR = -459.67;
 
     public static double toFahrenheit(double celsius) {
         if (celsius <= ABS_MIN_CELS) {
@@ -11,5 +12,15 @@ public class TempConversionService {
                     "No more life as we know it.");
         }
         return celsius * 9 / 5 + TEMP_DIFF;
+    }
+
+    public static double toCelsius(double fahrenheit) {
+        if (fahrenheit <= ABS_MIN_FAHR) {
+            System.out.println("You've reachet absolute ZERO temperature in FAHRENHEIT SCALE.\n" +
+                    "Atoms are stuck.\n" +
+                    "No more life as we know it.");
+        }
+
+        return (fahrenheit - TEMP_DIFF) * 5 / 9;
     }
 }
