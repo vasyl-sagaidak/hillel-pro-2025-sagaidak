c20ccedd5ff081d07c47ae00b9c6a81b830d1627
Vasyl Sagaidak <saikopixie@gmail.com>
2024-12-31 14:36:33 +0200

    Feature of Obj sout via static methods implemented

--- hillel_pro/src/main/java/lesson_04/app/Main.java
+++ hillel_pro/src/main/java/lesson_04/app/Main.java
@@ -2,6 +2,21 @@ package lesson_04.app;
 
 public class Main {
     public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "[Customer: " + customer.getName() +
+                ", phone: " + customer.getPhone() + "]";
+        getOutput(output);
+    }
 
+    public static String[] getData() {
+        return new String[]{"Be Svendsen", "+@$#^*&$^!@#"};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
     }
 }
