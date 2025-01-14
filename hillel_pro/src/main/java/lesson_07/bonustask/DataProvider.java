681dcdce64fa8dd7a482166aed2b7c22921b375e
Vasyl Sagaidak <saikopixie@gmail.com>
2025-01-14 18:41:49 +0200

    add bonus task for lesson 07

---
+++ hillel_pro/src/main/java/lesson_07/bonustask/DataProvider.java
@@ -0,0 +1,13 @@
+package lesson_07.bonustask;
+
+public class DataProvider {
+    public String[] getData() {
+        return new String[]{
+                "orange",
+                "plum",
+                "tomato",
+                "onion",
+                "grape",
+                "onion"};
+    }
+}
