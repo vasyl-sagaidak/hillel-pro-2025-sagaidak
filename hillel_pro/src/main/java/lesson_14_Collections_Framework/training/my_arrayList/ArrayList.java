3ddd766326125fdced14f7c02069182fca0e8883
Vasyl Sagaidak <saikopixie@gmail.com>
2025-02-17 17:35:32 +0200

    added initials for collection framework implementations

---
+++ hillel_pro/src/main/java/lesson_14_Collections_Framework/training/my_arrayList/ArrayList.java
@@ -0,0 +1,48 @@
+package lesson_14_Collections_Framework.training.my_arrayList;
+
+public class ArrayList<T> implements List<T> {
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public void add(T value, int index) {
+
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(T value, int index) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T remove(T element) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ hillel_pro/src/main/java/lesson_14_Collections_Framework/training/my_linkedList/MyLinkedList.java
@@ -0,0 +1,50 @@
+package lesson_14_Collections_Framework.training.my_linkedList;
+
+import java.util.List;
+
+public class MyLinkedList<T> implements MyLinkedListInterface<T>{
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public void add(T value, int index) {
+
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(T object) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
