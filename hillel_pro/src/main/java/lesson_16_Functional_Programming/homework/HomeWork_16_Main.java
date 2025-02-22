b08a17c69eae401fbd831abc4d79640cd229499d
Vasyl Sagaidak <saikopixie@gmail.com>
2025-02-22 23:18:44 +0200

    homework 16 complete

---
+++ hillel_pro/src/main/java/lesson_16_Functional_Programming/homework/HomeWork_16_Main.java
@@ -0,0 +1,67 @@
+package lesson_16_Functional_Programming.homework;
+
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class HomeWork_16_Main {
+    public static void main(String[] args) {
+// 1) Створіть функціональний інтерфейс MathOperation,
+//    який має один метод int operate(int a, int b).
+
+        // FunctionalInterfaces: MathOperation, StringManipulator are SUCCESSFULLY created!!!
+        // Classes: RandomNumberGenerator, StringListProcessor are SUCCESSFULLY created!!!
+
+// 2) Використовуючи анонімний клас, створіть об'єкт інтерфейсу MathOperation
+//    який виконує операцію додавання.
+
+        MathOperation mathOperation = new MathOperation() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        };
+
+        getOutput(mathOperation.operate(5, 3));
+
+// 3) Створіть лямбда-вираз, який реалізує функціональний інтерфейс StringManipulator
+//    і перетворює вхідний рядок в верхній регістр.
+
+        StringManipulator stringManipulator = (s) -> s.toUpperCase();
+
+        getOutput(stringManipulator.toUppercase("hillel java pro"));
+
+// 4) Створіть клас StringListProcessor, який має статичний метод int countUppercase(String s),
+//    що повертає кількість великих літер у рядку.
+
+        getOutput(StringListProcessor
+                .countUpperCase(" p P _aa R a@a$ D ii G -m M #@^& "));
+
+// 5) Використайте посилання на методи, щоб передати метод countUppercase як
+//    аргумент функції Function<String, Integer>.
+
+        Function<String, Integer> function = StringListProcessor::countUpperCase;
+
+        getOutput("Capital letters in word=VeRiTaS: "
+                + function.apply("VeRiTaS"));
+
+// 6) Створіть клас RandomNumberGenerator, який має статичний метод
+//    int generateRandomNumber(int min, int max), що повертає випадкове ціле число
+//    в заданому діапазоні.
+        getOutput(RandomNumberGenerator.generateRandomNumber(1,100));
+
+// 7) Використайте інтерфейс Supplier<T> для створення постачальника,
+//    який буде генерувати випадкове число від 1 до 100.
+        Supplier<Integer> supplier = () -> new Random().nextInt(100);
+
+        getOutput(supplier.get());
+    }
+
+    static void getOutput(Integer i) {
+        System.out.println(i);
+    }
+
+    static void getOutput(String s) {
+        System.out.println(s);
+    }
+}
