1789eedf1304c4b144c5702c36626309518a0111
Vasyl Sagaidak <saikopixie@gmail.com>
2025-02-22 23:07:32 +0200

    homework completed

--- hillel_pro/src/main/java/lesson_16_Functional_Programming/homework/HomeWork_16_Main.java
+++ hillel_pro/src/main/java/lesson_16_Functional_Programming/homework/HomeWork_16_Main.java
@@ -1,14 +1,16 @@
 package lesson_16_Functional_Programming.homework;
 
+import java.util.Random;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class HomeWork_16_Main {
     public static void main(String[] args) {
 // 1) Створіть функціональний інтерфейс MathOperation,
 //    який має один метод int operate(int a, int b).
 
-    // FunctionalInterfaces: MathOperation, StringManipulator are SUCCESSFULLY created!!!
-    // Classes: RandomNumberGenerator, StringListProcessor are SUCCESSFULLY created!!!
+        // FunctionalInterfaces: MathOperation, StringManipulator are SUCCESSFULLY created!!!
+        // Classes: RandomNumberGenerator, StringListProcessor are SUCCESSFULLY created!!!
 
 // 2) Використовуючи анонімний клас, створіть об'єкт інтерфейсу MathOperation
 //    який виконує операцію додавання.
@@ -20,13 +22,13 @@ public class HomeWork_16_Main {
             }
         };
 
-        UIOut.getOutput(mathOperation.operate(5,3));
+        UIOut.getOutput(mathOperation.operate(5, 3));
 
 // 3) Створіть лямбда-вираз, який реалізує функціональний інтерфейс StringManipulator
 //    і перетворює вхідний рядок в верхній регістр.
 
-        //StringManipulator stringManipulator = String::toUpperCase;
         StringManipulator stringManipulator = (s) -> s.toUpperCase();
+
         UIOut.getOutput(stringManipulator.toUppercase("hillel java pro"));
 
 // 4) Створіть клас StringListProcessor, який має статичний метод int countUppercase(String s),
@@ -40,14 +42,18 @@ public class HomeWork_16_Main {
 
         Function<String, Integer> function = StringListProcessor::countUpperCase;
 
+        UIOut.getOutput("Capital letters in word=VeRiTaS: "
+                + function.apply("VeRiTaS"));
+
 // 6) Створіть клас RandomNumberGenerator, який має статичний метод
 //    int generateRandomNumber(int min, int max), що повертає випадкове ціле число
 //    в заданому діапазоні.
+        UIOut.getOutput(RandomNumberGenerator.generateRandomNumber(1,100));
 
 // 7) Використайте інтерфейс Supplier<T> для створення постачальника,
 //    який буде генерувати випадкове число від 1 до 100.
+        Supplier<Integer> supplier = () -> new Random().nextInt(100);
 
+        UIOut.getOutput(supplier.get());
     }
-
-
 }
--- hillel_pro/src/main/java/lesson_16_Functional_Programming/homework/RandomNumberGenerator.java
+++ hillel_pro/src/main/java/lesson_16_Functional_Programming/homework/RandomNumberGenerator.java
@@ -1,10 +1,14 @@
 package lesson_16_Functional_Programming.homework;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class RandomNumberGenerator {
     public static int generateRandomNumber(int min, int max) {
-//        return new Random().nextInt(min, max);
-        return 0;
+        if (min > max) {
+            throw new IllegalArgumentException(
+                    "Min value can't be bigger than max value");
+        }
+        SecureRandom random = new SecureRandom();
+        return random.nextInt((max - min) + 1) + min;
     }
 }
