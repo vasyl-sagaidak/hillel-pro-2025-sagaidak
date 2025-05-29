715f7ccea6e382db5bf58e37935460a0eb6eea1d
Vasyl Sagaidak <saikopixie@gmail.com>
2025-05-29 14:40:59 +0300

    homework39 implemented

---
+++ hillel_pro/spring-jdbc-test/spring-jdbc/src/main/java/org/example/springjdbc/dto/Customer.java
@@ -0,0 +1,20 @@
+package org.example.springjdbc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class Customer {
+    private int id;
+    private String fullName;
+    private String email;
+    private int socialSecurityNumber;
+
+    public Customer() {
+    }
+}
